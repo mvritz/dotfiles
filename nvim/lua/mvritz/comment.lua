97163fc52ead6667020be11465e3c64cce3430b4
mvritz <moritz.dresch@titanom.com>
2024-07-17 17:58:09 +0200

    feat: gitignore

--- nvim/lua/mvritz/comment.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-M.setup = function()
-  require('Comment').setup({
-    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-  })
-end
-
-return M
--- nvim/lua/mvritz/neoscroll.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M.setup = function()
-  require('neoscroll').setup()
-end
-
-return M
